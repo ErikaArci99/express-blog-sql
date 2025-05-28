71196095bcb3ba92a1b22e3d2e3f43eee3657d8b
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-28 17:04:20 +0200

    [UPD] creato controllers per index

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,9 +1,14 @@
-const index = (req, res) => {
-    console.log('elenco post')
-};
+const connection = require('../data/db');
 
-const show = (req, res) => {
-    console.log('post con id' + req.params.id)
+const index = (req, res) => {
+    const sql = 'SELECT * FROM posts';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        res.json(results);
+    });
 };
 
 module.exports = { index, show };

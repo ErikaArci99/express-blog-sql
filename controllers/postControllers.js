886f04aef5813cf9db61d0cedc581ff935e90665
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-28 15:22:04 +0200

    [UPD] creati i controllers e esportati

---
+++ controllers/postControllers.js
@@ -0,0 +1,9 @@
+const index = (req, res) => {
+    console.log('elenco post')
+};
+
+const show = (req, res) => {
+    console.log('post con id' + req.params.id)
+};
+
+module.export = { index, show };

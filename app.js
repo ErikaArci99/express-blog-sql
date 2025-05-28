6884e103cc6e095f11b0abd094730612c5ca37e0
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-28 14:58:46 +0200

    [UPD] importato express e avviato server nella porta 3000,  testato anche su postman

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log('server del mio blog')
+    res.send('benvenuto nel mio blog')
+});
+
+app.listen(port, () => {
+    console.log(`server in ascolto sulla porta ${port}`)
+});
